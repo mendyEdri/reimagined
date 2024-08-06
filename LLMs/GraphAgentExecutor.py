5a75439e47e0a41f7c03a1f898f3510e407fece0
Mendy Edri <mendye@wix.com>
2024-08-06 08:15:28 +0300

    personal details - stub

--- LLMs/GraphAgentExecutor.py
+++ LLMs/GraphAgentExecutor.py
@@ -4,23 +4,29 @@ from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_core.messages import SystemMessage, HumanMessage
 
 class GraphAgentExecutor:
-
+    
     default_llm = ChatOpenAI(model='gpt-4o-mini', temperature=0)
 
-    def get_agent(self, llm=default_llm, tools=[], system="", next_options=[], variable_placeholder="messages"):
+    def __init__(self, llm=default_llm, tools=[], system="", next_options=[], variable_placeholder="messages"):
         llm = ChatOpenAI(model='gpt-4o-mini', temperature=0)
 
         prompt = ChatPromptTemplate.from_messages([
             SystemMessage(content=system), 
             MessagesPlaceholder(variable_name=variable_placeholder), 
             SystemMessage(content=f"""
-                Given the conversation above, who should act next?
-                Or should we FINISH? select one of: {next_options}
+                You are a supervisor tasked with managing a conversation between the 
+                following workers: {next_options}. Given the following user request,
+                respond with the worker to act next. Each worker will perform a
+                task and respond with their results and status. When finished,
+                respond with FINISH.
                 """),
             MessagesPlaceholder(variable_name="agent_scratchpad")
         ])
 
         agent = create_tool_calling_agent(llm, tools, prompt)
-        model = AgentExecutor(agent=agent, tools=tools, verbose=True)
+        self.model = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-        return model
+    def should_continue(self, state):
+        print(state)
+        
+        
